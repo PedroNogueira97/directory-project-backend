df6568800bf97dafbf900991459feba5b8811adb
Pedro Nogueira <pedro.neto@arcasolutions.com>
2024-08-05 22:25:46 -0300

    First commit

---
+++ directory-backend/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'myprojectpassword',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
